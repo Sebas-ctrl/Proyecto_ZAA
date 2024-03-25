e07aa0e89d491589002a62f9b58f613a3b3b0fa4
Amisadai <210752@utxicotepec.com>
2024-03-24 20:27:36 -0600

    Creacion de archivo procedimiento de recursos materiales, para el llenado de la tabla equipos

---
+++ DataBase/SQL/Procedimiento_Recursos_Materiales.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+
+CREATE PROCEDURE llenar_tabla_equipos()
+BEGIN
+    -- Insertar datos en la tabla equipos
+    INSERT INTO equipos (nombre_del_equipo, numero_de_serie, marca, modelo)
+    VALUES 
+        ('Caminadora', 'SER123', 'MarcaX', 'ModeloA'),
+        ('Bicicleta estática', 'SER456', 'MarcaY', 'ModeloB'),
+        ('Máquina de pesas', 'SER789', 'MarcaZ', 'ModeloC');
+END //
+
+DELIMITER ;
