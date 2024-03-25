0e7eb41ba2c1966d39bc0a9aed9b2e58a36a2014
Amisadai <210752@utxicotepec.com>
2024-03-24 20:36:25 -0600

    Agregacion del procedimiento de mantenimientos

--- DataBase/SQL/Procedimiento_Recursos_Materiales.sql
+++ DataBase/SQL/Procedimiento_Recursos_Materiales.sql
@@ -11,3 +11,14 @@ BEGIN
 END //
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE llenar_tabla_mantenimiento()
+BEGIN
+    -- Insertar datos en la tabla mantenimiento con valores predeterminados o funciones
+    INSERT INTO mantenimiento (descripcion_del_trabajo_realizado, fecha_de_mantenimiento, estado_del_equipo_despues_del_mantenimiento)
+    VALUES ('Trabajo realizado automáticamente', CURRENT_DATE(), 'Excelente');
+END //
+
+DELIMITER ;
